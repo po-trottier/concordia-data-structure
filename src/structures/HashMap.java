b7885c491c2d61fe779a690469711b6a2a6d7772
Pierre-Olivier Trottier <po.trottier@gmail,com>
2019-11-27 18:33:19 -0500

    Added Basic Layout for the HashMap Class

--- src/structures/HashMap.java
+++ src/structures/HashMap.java
@@ -3,6 +3,8 @@ package structures;
 // LinkedHashMap Documentation:
 // https://docs.oracle.com/javase/8/docs/api/java/util/LinkedHashMap.html
 import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javafx.util.Pair;
 
 public class HashMap {
@@ -19,6 +21,40 @@ public class HashMap {
     for (String key : keys) {
       this.internalMap.put(key, sequence.get(key));
     }
+  }
+
+  public Pair[] get(String key) {
+    return internalMap.get(key);
+  }
+
+  public void add(String key, Pair<String, Object> value) {
+    if (internalMap.get(key) != null) {
+      ArrayList<Pair> data = new ArrayList<>(Arrays.asList(internalMap.get(key)));
+      data.add(value);
+      internalMap.remove(key);
+      internalMap.put(key, data.toArray(new Pair[0]));
+    } else {
+      internalMap.put(key, new Pair[] { value });
+    }
+  }
+
+  public void remove(String key) {
+
+  }
+
+  public String[] allKeys() {
+
+    return new String[0];
+  }
+
+  public String nextKey(String key) {
+
+    return key;
+  }
+
+  public String prevKey(String key) {
+
+    return key;
   }
 
   public boolean isEmpty() {
